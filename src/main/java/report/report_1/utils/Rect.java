8706fca34ef41ac162a90ca35ce0c5f82d0772e9
daichi904 <minimi.904.horo@gamil.com>
2025-06-10 12:50:44 +0900

    run checkAll

--- src/main/java/report/report_1/utils/Rect.java
+++ src/main/java/report/report_1/utils/Rect.java
@@ -1,12 +1,12 @@
 package report.report_1.utils;
 
 public class Rect {
-    public Coordinate p1, p2, p3, p4;
+  public Coordinate p1, p2, p3, p4;
 
-    public Rect(Coordinate p1, Coordinate p2, Coordinate p3, Coordinate p4) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.p3 = p3;
-        this.p4 = p4;
-    }
+  public Rect(Coordinate p1, Coordinate p2, Coordinate p3, Coordinate p4) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+    this.p4 = p4;
+  }
 }
